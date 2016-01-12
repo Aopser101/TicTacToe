033abd0fe8a1834550686aeafb3746764ce60f5f
Aopser101 <ic3kreem@gmail.com>
2016-01-12 09:51:49 -0500

    Create TTT.java

---
+++ TTT.java
@@ -0,0 +1,25 @@
+public class TicTacToe
+{
+    int[][] board = new int[3][3];
+    private int x;
+    
+    public TicTacToe()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+    
+    public static Pair<Integer> bestMove(){
+        int a,b=0;
+        int q1,q2;
+        for(a=0;a<=2;a++){
+            for(b=0;b<=2;b++){
+                
+            }
+        }
+    }
+    public void makeMove()
+    {
+        s
+    }
+}
